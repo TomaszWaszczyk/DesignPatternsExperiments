e664706a23ff0d3038b621235f812d28a793a4b3
Tomasz Waszczyk <tomasz@waszczyk.com>
2014-07-20 19:39:27 +0200

    [Added] Comments on singleton pattern.

--- src/main/java/com/waszczyk/designpatterns/creational/singleton/AmericaPresident.java
+++ src/main/java/com/waszczyk/designpatterns/creational/singleton/AmericaPresident.java
@@ -1,5 +1,15 @@
 package com.waszczyk.designpatterns.creational.singleton;
 
+
+/*
+ *  Singleton has three elements: 
+ *  
+ * 	+ private constructor - no other class can instantiate a new object.
+	+ private reference - no external modification.
+	+ public static method is the only place that can get an object.
+ * 
+ * 
+ */
 public class AmericaPresident {
 
 	private static final AmericaPresident thePresident = new AmericaPresident();
